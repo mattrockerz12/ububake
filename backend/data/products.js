d0f2311a5ae210fcc8da9c690aa8320239e8d2d3
mattrockerz12 <mattrockerz35@gmail.com>
2021-06-26 23:27:01 +0400

    first commit

---
+++ backend/data/products.js
@@ -0,0 +1,68 @@
+const products = [
+    {
+        name: 'Sugar-Free Gluten-Free Chocolate Chip Cookies',
+        image: 'https://cdn.shopify.com/s/files/1/0243/8437/2816/products/3_2000x.png?v=1594458927',
+        description: `An oldie but a goodie, nothing beats our Sugar-Free Gluten-Free Chocolate Chip Cookies!
+        An oldie but a goodie, nothing beats our Sugar-Free Gluten-Free Chocolate Chip Cookies!
+        We revamped a well-loved classic, this time using almond flour, rich sugar-free chocolate chips and just the right hint of vanilla. enjoy a bite from the past, 
+        minus the guilt!   
+        Just the right mix of chewy and crispy in each bite, ooey and gooey done right!    
+        100% Keto Approved
+        Sugar-Free (sweetened with Erythritol)
+        Diabetic-Friendly
+        Gluten-Free`,
+        category: 'Cookies',
+        price: 300,
+        countInStock: 10,
+        rating: 4.0,
+        numReviews: 4
+    },
+    {
+        name: 'Keto Pandesal',
+        image: 'https://cdn.shopify.com/s/files/1/0243/8437/2816/products/ketopandesal_2000x.jpg?v=1583540395',
+        description: `Kick start your morning routine with our Keto Pandesal.
+        100% Keto Approved
+        Sugar-Free (sweetened with Erythritol)
+        Gluten-Free
+        Low-Carb
+        Diabetic-Friendly
+        bag of 6 keto pandesal`,
+        category: 'Bread',
+        price: 250,
+        countInStock: 5,
+        rating: 5,
+        numReviews: 3
+    },
+    {
+        name: 'Keto Choco Hazelnut Spread',
+        image: 'https://cdn.shopify.com/s/files/1/0243/8437/2816/products/ketopandesal_2000x.jpg?v=1583540395',
+        description: `Make this part of your daily breakfast spread. Made of 100% hazelnuts!
+        100% Keto Approved
+        Sugar-Free (sweetened with Erythritol)
+        Gluten-Free
+        Low-Carb
+        Diabetic-Friendly
+        Keto Choco Hazelnut Spread   
+        140 grams for P275`,
+        category: 'Chocolate',
+        price: 275,
+        countInStock: 3,
+        rating: 5,
+        numReviews: 5
+    },
+    {
+        name: 'Sugar-Free Fresh Blueberry Cake',
+        image: 'https://cdn.shopify.com/s/files/1/0243/8437/2816/products/Sugar-Free_Fresh_Blueberry_Cake_2000x_d0ec0436-d045-44af-82e2-ad911a4a3f51_2000x.jpg?v=1596522661',
+        description: `How can a cake so light, be so richly dark and chocolaty? One bite, and the mystery behind the 
+        riddle will still remain unsolved. But what we can tell you is that our infusion of fresh blueberries does wonders for this spongy chiffon. 
+        This is the kind of cloudy softness angels would sleep on.',
+        category: 'Chocolate`,
+        category: 'Cake',
+        price: 275,
+        countInStock: 8,
+        rating: 3,
+        numReviews: 5
+    },
+]
+
+export default products
